71061d88d6a8818ba85f996b936028a363071c3c
hnd <hnd@192.168.2.21>
2022-07-01 11:47:31 +0700

    Time helper version 1

---
+++ src/helpers/time.helper.ts
@@ -0,0 +1,33 @@
+import {
+  endOfQuarter,
+  getQuarter,
+  startOfQuarter,
+  startOfMonth,
+  endOfMonth,
+  Day,
+  compareDesc,
+} from "date-fns";
+export class TimeHelper {
+  public static getQuarterOfYear = (date: Date) => getQuarter(date);
+
+  public static firstDateOfQuarter = (date: Date) => startOfQuarter(date);
+
+  public static lastDateOfQuarter = (date: Date) => endOfQuarter(date);
+
+  public static firstDateOfMonth = (date: Date) => startOfMonth(date);
+
+  public static lastDateOfMonth = (date: Date) => endOfMonth(date);
+
+  public static calculateBusinessDay = (
+    firstDate: Date,
+    lastDate: Date,
+    daysOffInWeeks: Day[]
+  ) => {
+    if (compareDesc(firstDate, lastDate)) {
+      return -1;
+    }
+  };
+}
+
+const date = new Date("2022-01-15");
+console.log(TimeHelper.firstDateOfQuarter(date).getDate());
