da7f0e90bfbfe1f132fe8c5100a91138ebca8f0f
Jerry Nguyen <jerry.nguyen@shopback.com>
2022-10-23 11:30:57 +0700

    Migration, change to use configService instead of local config in be-core

--- src/modules/migration/migration.service.ts
+++ src/modules/migration/migration.service.ts
@@ -1,32 +1,35 @@
 import { Inject, Injectable, InternalServerErrorException, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { isArray } from 'lodash';
 import * as mysql from 'mysql2/promise';
 import { DataSource } from 'typeorm';
-import { get } from '~/config';
+import { DatabaseOption } from '~/models/common.model';
 import { MODULE_OPTIONS_TOKEN } from '~/modules/migration/const';
 import AddMigration from '~/modules/migration/dtos/add-migration.dto';
 import { MigrationOption } from '~/modules/migration/type';
-
-const tenantOption = get('tenantSQLConnection');
-const serviceOption = get('primarySQLConnection');
 
 @Injectable()
 export class MigrationService implements OnModuleInit {
     private tenantPool: mysql.Pool;
     private servicePool: mysql.Pool;
-    constructor(@Inject(MODULE_OPTIONS_TOKEN) private option: MigrationOption) {
+    private tenantOption: DatabaseOption;
+    private serviceOption: DatabaseOption;
+    constructor(@Inject(MODULE_OPTIONS_TOKEN) private option: MigrationOption, configService: ConfigService) {
+        this.tenantOption = configService.get<DatabaseOption>('tenantSQLConnection') as DatabaseOption;
+        this.serviceOption = configService.get<DatabaseOption>('primarySQLConnection') as DatabaseOption;
+
         this.tenantPool = mysql.createPool({
-            host: tenantOption['host'],
-            port: tenantOption['port'],
-            user: tenantOption['username'],
-            database: tenantOption['database'],
-            password: tenantOption['password'],
+            host: this.tenantOption['host'],
+            port: this.tenantOption['port'],
+            user: this.tenantOption['username'],
+            database: this.tenantOption['database'],
+            password: this.tenantOption['password'],
         });
         this.servicePool = mysql.createPool({
-            host: serviceOption['host'],
-            port: serviceOption['port'],
-            user: serviceOption['username'],
-            password: serviceOption['password'],
+            host: this.serviceOption['host'],
+            port: this.serviceOption['port'],
+            user: this.serviceOption['username'],
+            password: this.serviceOption['password'],
         });
     }
     async onModuleInit() {
@@ -59,14 +62,14 @@ export class MigrationService implements OnModuleInit {
     }
 
     private async migration(tenantCode: string) {
-        const db: string = serviceOption['password'].replace(/{0}/g, tenantCode);
+        const db: string = this.serviceOption['password'].replace(/{0}/g, tenantCode);
         const conn = await this.servicePool.getConnection();
         const dataSource = new DataSource({
-            type: serviceOption['type'],
-            host: serviceOption['host'],
-            port: serviceOption['port'],
-            password: serviceOption['password'],
-            username: serviceOption['username'],
+            type: this.serviceOption['type'],
+            host: this.serviceOption['host'],
+            port: this.serviceOption['port'],
+            password: this.serviceOption['password'],
+            username: this.serviceOption['username'],
             database: db,
             migrations: [this.option.migrationDir],
             synchronize: false,
